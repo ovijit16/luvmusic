e12b59f0e8a31e10ca1cc94d0d402557c29694bb
ansarker <shahidsarker95@hotmail.com>
2018-03-15 16:31:57 +0000

    back-end touch 1

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -4,15 +4,22 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('./../index.ejs', {
+	res.render('./../public/views/index.ejs', {
 		user: req.user || null,
 		request: req
 	});
 };
 
 exports.about = function(req, res) {
-	res.render('./../about.ejs', {
+	res.render('./../public/views/about.ejs', {
 		user: req.user || null,
 		request: req
 	});
 };
+
+exports.contact = function(req, res) {
+	res.render('./../public/views/contact.ejs', {
+		user: req.user || null,
+		request: req
+	});
+}
